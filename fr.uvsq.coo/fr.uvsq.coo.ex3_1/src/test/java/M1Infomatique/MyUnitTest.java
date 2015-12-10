a5f72c45db325bf1714d2c852f558e295beae02a
uvsq21507627 <kenny.blondy@laposte.net>
2015-12-10 08:45:39 +0100

    Add ex3_1

---
+++ fr.uvsq.coo/fr.uvsq.coo.ex3_1/src/test/java/M1Infomatique/MyUnitTest.java
@@ -0,0 +1,18 @@
+package M1Infomatique;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyUnitTest {
+	
+	  @Test
+	    public void testConcatenate() {
+	        MyUnit myUnit = new MyUnit();
+
+	        String result = myUnit.concatenate("one", "two");
+
+	        assertEquals("onetwo", result);
+
+	    }
+
+}
